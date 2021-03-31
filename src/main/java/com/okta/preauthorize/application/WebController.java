25797aaf3e7892853feea98577ed9bf7036f792f
Matt Raible <matt.raible@okta.com>
2021-03-31 14:49:46 -0600

    Upgrade PreAuthorize example to use Spring Boot 2.4.4

--- src/main/java/com/okta/preauthorize/application/WebController.java
+++ src/main/java/com/okta/preauthorize/application/WebController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
-
 @Controller
 public class WebController {
 
@@ -16,11 +14,11 @@ public class WebController {
         return "Welcome home!";
     }
 
-    @PreAuthorize("hasAuthority('Admin')")
     @RequestMapping("/restricted")
     @ResponseBody
+    @PreAuthorize("hasAuthority('Admin')")
     public String restricted() {
         return "You found the secret lair!";
     }
-    
+
 }
